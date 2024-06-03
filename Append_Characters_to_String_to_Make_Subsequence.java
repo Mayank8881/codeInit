f08fa625451f2d6685d076c297767a75838aeaec
Mayank8881 <mayankmokhere2527@gmail.com>
2024-06-03 16:58:26 +0530

    Add Append Characters to String to Make Subsequence

--- Append_Characters_to_String_to_Make_Subsequence.java
+++ Append_Characters_to_String_to_Make_Subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public int appendCharacters(String s, String t) {
-        int i,j,count=-1,m=0;
+        int i,j,count=1,m=0;
         for(i=0;i<t.length();i++){
             char c=t.charAt(i);
             for(j=i;j<s.length();j++){
@@ -17,18 +17,12 @@ class Solution {
                 }
             
             }
-            if(count!=-1){
+            if(count!=1){
                 break;
             }
             
 
         }
-        if(count==-1){
-            return 0;
-        }
-        else{
-            return (t.length()-count);
-
-        }
+        return (t.length()-count);
     }
 }
