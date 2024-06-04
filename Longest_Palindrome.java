aed193dd57354bd350734e3a11aeb9728c40ed59
Mayank8881 <mayankmokhere2527@gmail.com>
2024-06-04 16:41:45 +0530

    Add Longest Palindrome

--- Longest_Palindrome.java
+++ Longest_Palindrome.java
@@ -4,23 +4,17 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
             int count=0;
-            int flag=0;
             if(i>0){
-                for(int k=i;k>=0;k--){
-                    if(c==s.charAt(k)){
-                        flag=1;
-                    }
-                }
-                if(flag==0){
+                if(c!=s.charAt(i-1)){
                     for(int j=i;j<s.length();j++){
                         if(s.charAt(j)==c){
                                 count++;
                                 System.out.println(count);
                                 System.out.println();
-                        }
+                            }
                     
-                    }   
-                }
+                        }   
+                    }
 
             }
             else{
