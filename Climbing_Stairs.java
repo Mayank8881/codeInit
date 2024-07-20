c0b59d698076c3676844b80ac48e224815db3e4f
Mayank8881 <mayankmokhere2527@gmail.com>
2024-07-20 17:26:24 +0530

    Add Climbing Stairs

--- Climbing_Stairs.java
+++ Climbing_Stairs.java
@@ -1,13 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==0 || n==1) return n;
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if (n == 0) {
+            return 1;
         }
-        return dp[n];
+        else if (n < 0) {
+            return 0;
+        }
+        return climbStairs(n - 1)+climbStairs(n - 2);
+    
     }
 }
