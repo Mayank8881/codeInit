60ec62c35affcb26647050d9f079ca299faab80a
Mayank8881 <mayankmokhere2527@gmail.com>
2024-07-30 17:28:51 +0530

    Add Minimum Deletions to Make String Balanced

---
+++ Minimum_Deletions_to_Make_String_Balanced.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int minimumDeletions(String s) {
+        int n=s.length();
+        int arr[]=new int[n];
+        int acount=0;
+        int bcount=0;
+        for(int i=n-1;i>=0;i--){
+            arr[i]=acount;
+            if(s.charAt(i)=='a'){
+                acount++;
+            }
+        } 
+        int global_count=1000000;
+        for(int i=0;i<n;i++){
+            global_count=Math.min(global_count,bcount+arr[i]);
+            if(s.charAt(i)=='b'){
+                bcount++;
+            }
+        }
+        return global_count;
+
+    }
+}
