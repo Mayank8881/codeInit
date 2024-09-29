89e76d3eb781c71251a1955582b30acaccd4eda5
Mayank8881 <mayankmokhere2527@gmail.com>
2024-09-29 09:34:08 +0530

    Add Rotate Array

--- Rotate_Array.java
+++ Rotate_Array.java
@@ -1,17 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n=nums.length;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
-    }
-    public static void reverse(int arr[],int l,int r){
-        while(l<=r){
-            int temp=arr[l];
-            arr[l]=arr[r];
-            arr[r]=temp;
-            l++;
-            r--;
+        k%=nums.length;
+        // System.out.println(k);
+        for(int i=0;i<k;i++){
+            int temp=nums[nums.length-1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0]=temp;
+
         }
-    } 
+
+        // for(int m=0;m<nums.length;m++){
+        //     System.out.print(nums[m]+",");
+        // }
+        
+    }
 }
