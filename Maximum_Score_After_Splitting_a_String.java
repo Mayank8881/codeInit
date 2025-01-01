3b1e3cdbff79f15389de7034a5668c2bea5228c1
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-01 14:15:19 +0530

    Add Maximum Score After Splitting a String

--- Maximum_Score_After_Splitting_a_String.java
+++ Maximum_Score_After_Splitting_a_String.java
@@ -1,30 +1,23 @@
 class Solution {
     public int maxScore(String s) {
         int l=0,r=0,maxSum=0;
-        
         while(r<s.length()){
-            int count0=0,count1=0,sum=0,tSum=0;
+            int count=0,sum=0,tSum=0;
             for(int i=l;i<=r;i++){
                 if(s.charAt(i)=='0'){
-                    count0++;
-                }
-                if(count0==s.length()){
-                    return 1;
+                    count++;
                 }
             }
 
             for(int j=r+1;j<s.length();j++){
                 if(s.charAt(j)=='1'){
-                    count1++;
-                }
-                if(count1==s.length()){
-                    return 1;
+                    count++;
                 }
             }
 
-            tSum=count0+count1;
+            // tSum=sum+count;
             // System.out.println(count);
-            maxSum=Math.max(tSum,maxSum);
+            maxSum=Math.max(count,maxSum);
             r++;
         }
         return maxSum;
