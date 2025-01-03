b1bccaa721dedfab4b54280bc690301a5bfe822e
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-03 18:37:01 +0530

    Add Number of Ways to Split Array

--- Number_of_Ways_to_Split_Array.java
+++ Number_of_Ways_to_Split_Array.java
@@ -4,13 +4,11 @@ class Solution {
         int count=0;
         for(int i=0;i<n-1;i++){
             int lSum=0,rSum=0;
-            for(int j=0;j<n;j++){
-                if(j<=i){
-                    lSum+=nums[j];
-                }
-                else{
-                    rSum+=nums[j];
-                }
+            for(int j=0;j<=i;j++){
+                lSum+=nums[j];
+            }
+            for(int k=i+1;k<n;k++){
+                rSum+=nums[k];
             }
             if(lSum>=rSum){
                 count++;
