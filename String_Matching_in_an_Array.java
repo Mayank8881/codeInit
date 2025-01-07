aec5ceebcabdcb0be8c2a1f4d21afd768e97b914
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-07 16:35:51 +0530

    Add String Matching in an Array

---
+++ String_Matching_in_an_Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> str=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words.length;j++){
+                if(i!=j && words[j].contains(words[i])){
+                    str.add(words[i]);
+                }
+            }
+        }
+        return str;
+    }
+}
