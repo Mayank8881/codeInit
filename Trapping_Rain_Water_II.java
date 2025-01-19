a7cf55033bc1d442fff355ec65acbd66fa000a8b
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-19 17:52:21 +0530

    Add Trapping Rain Water II

--- Trapping_Rain_Water_II.java
+++ Trapping_Rain_Water_II.java
@@ -43,7 +43,6 @@ class Solution {
                 } else {
                     pq.offer(new int[]{heightMap[newRow][newCol], newRow, newCol});
                 }
-                visited[newRow][newCol] = true;
             }
         }
         return water;
