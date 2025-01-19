ffdcdac97618a80053027e4f8961a2d298b64a6d
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-19 13:11:38 +0530

    Add Longest Common Prefix

--- Longest_Common_Prefix.java
+++ Longest_Common_Prefix.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb=new StringBuilder("");
+        //StringBuilder bhi use kr skteee hehehe
         Arrays.sort(strs);
         String s1=strs[0];
         String s2=strs[strs.length-1];
@@ -8,13 +8,12 @@ class Solution {
 
         while(idx<s1.length() && idx<s2.length()){
             if(s1.charAt(idx)==s2.charAt(idx)){
-                sb.append(s1.charAt(idx));
                 idx++;
             }
             else{
                 break;
             }
         }
-        return sb.toString();
+        return s1.substring(0,idx);
     }
 }
