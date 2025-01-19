c353c6d68f82002ae9711f93f21c786c3986991c
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-19 11:32:29 +0530

    Add Remove Outermost Parentheses

--- Remove_Outermost_Parentheses.java
+++ Remove_Outermost_Parentheses.java
@@ -1,18 +1,17 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        // Stack<Character> br=new Stack<>();
-        int count=0;
+        Stack<Character> br=new Stack<>();
         StringBuilder sb=new StringBuilder("");
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                if(count>0){
+                if(br.size()>0){
                     sb.append(s.charAt(i));
                 }
-                count++;
+                br.push(s.charAt(i));
             }
             else{
-               count--;
-                if(count>0){
+               br.pop();
+                if(br.size()>0){
                     sb.append(s.charAt(i));                    
                 }
             }
