f284636391ad5e868232fe9639047bb95e42d948
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-19 12:47:30 +0530

    Add Largest Odd Number in String

--- Largest_Odd_Number_in_String.java
+++ Largest_Odd_Number_in_String.java
@@ -1,29 +1,20 @@
 class Solution {
     public String largestOddNumber(String num) {
-        char c=num.charAt(num.length()-1);
-        int n=c-'0';
         int sum=0;
-        if(n%2!=0){
-            return num;
-        }
-        else{
-            for(int i=num.length()-2;i>=0;i--){
-                char ch=num.charAt(i);
-                int nn=ch-'0';
-                if(nn%2!=0){
-                   if(nn>sum){
-                    sum=nn;
-                   }
-                }
-            }
-            if(sum==0){
-                return "";
+        num = num.replaceAll("0+$", "");
+
+        for(int i=num.length()-1;i>=0;i--){
+            int c=num.charAt(i);
+            int n=c-'0';
+            if(n%2!=0){
+                num = num.substring(0, i+1);
+                break;
             }
             else{
-                return String.valueOf(sum);
+                num = num.substring(0, i);
             }
-            
         }
+        return num;
         
     }
 }
