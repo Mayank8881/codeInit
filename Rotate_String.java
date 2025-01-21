dd489e99ffaf62020dc126b51423fc6b15719d5a
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-21 19:58:20 +0530

    Add Rotate String

--- Rotate_String.java
+++ Rotate_String.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
-            return false;
-        }
         String str=s+s;
         if(str.contains(goal)){
             return true;
