6f1d1e8d1eef22ee1604865f53f2e9f4160a266c
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-21 20:08:11 +0530

    Add Valid Anagram

--- Valid_Anagram.java
+++ Valid_Anagram.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
         HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++){
             if(map.containsKey(s.charAt(i))){
