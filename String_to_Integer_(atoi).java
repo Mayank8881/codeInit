e2b052f7f4151af726c942b942a29ef1e035d2d3
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-27 16:43:46 +0530

    Add String to Integer (atoi)

--- String_to_Integer_(atoi).java
+++ String_to_Integer_(atoi).java
@@ -1,62 +1,28 @@
 class Solution {
-    // public int myAtoi(String s) {
-    //     int i=0;
-    //     int n=s.length();
-    //     while(i<n && s.charAt(i)==' '){
-    //         i++;
-    //     }
-
-    //     int sign=1;
-    //     if(i<n && (s.charAt(i)=='-' || s.charAt(i)=='+')){
-    //         if(s.charAt(i)=='-'){
-    //             sign=-1;
-    //         }
-    //         i++;
-    //     }
-    //     int num=0;
-    //     while(i<n && Character.isDigit(s.charAt(i))){
-    //         int digit=s.charAt(i)-'0';
-
-    //         if (num > (Integer.MAX_VALUE - digit) / 10) {
-    //             return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-    //         }
-    //         num=num*10+digit;
-    //         i++;
-    //     }
-    //     return sign*num;
-    // }
-
     public int myAtoi(String s) {
-        s=s.trim();
-        if(s.isEmpty()){
-            return 0;
+        int i=0;
+        int n=s.length();
+        while(i<n && s.charAt(i)==' '){
+            i++;
         }
 
-        boolean negative=false;
-        int index=0;
-
-        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
-            negative = s.charAt(0) == '-';
-            index++;
+        int sign=1;
+        if(i<n && (s.charAt(i)=='-' || s.charAt(i)=='+')){
+            if(s.charAt(i)=='-'){
+                sign=-1;
+            }
+            i++;
         }
+        int num=0;
+        while(i<n && Character.isDigit(s.charAt(i))){
+            int digit=s.charAt(i)-'0';
 
-        return helper(s,index,negative,0);
-    }
-
-    public static int helper(String s,int index,boolean negative,int result){
-        
-        if(index >= s.length() || !Character.isDigit(s.charAt(index))) {
-            return negative ? -result : result;
+            if (num > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            num=num*10+digit;
+            i++;
         }
-        
-        int digit=s.charAt(index)-'0';
-
-        if (result > (Integer.MAX_VALUE - digit) / 10) {
-            return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        }
-
-        result=result*10+digit;
-        return helper(s,index+1,negative,result);
-
+        return sign*num;
     }
 }
