4d45bf50d604408fbeac3b194ff37d1d9498156d
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-27 15:38:58 +0530

    Add Maximum Nesting Depth of the Parentheses

---
+++ Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxCount=0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                count++;
+            }
+            else if(s.charAt(i)==')'){
+                count--;
+            }
+            maxCount=Math.max(count,maxCount);
+        }
+        return maxCount;
+
+    }
+}
