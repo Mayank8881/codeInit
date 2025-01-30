198c1c4cecd1aeed0d77d4a8d5baada5ff37af70
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-30 08:09:28 +0530

    Add Pow(x, n)

--- Pow(x,_n).java
+++ Pow(x,_n).java
@@ -1,20 +1,5 @@
 class Solution {
-    public double myPow(double x, int n) {
-        if(n==0){
-            return 1;
-        }
-        if(n<0){
-            n=-n;
-            x=1/x;
-            return x*myPow(x,n-1);
-        }
-        double ans=myPow(x,n/2);
-        if(n%2==0){
-            return ans*ans;
-        }
-        return x*ans*ans;
-    }
-    // public double myPow(double x,int n){
+    // public double myPow(double x, int n) {
     //     if(n==0){
     //         return 1;
     //     }
@@ -23,11 +8,26 @@ class Solution {
     //         x=1/x;
     //         return x*myPow(x,n-1);
     //     }
+    //     double ans=myPow(x,n/2);
     //     if(n%2==0){
-    //         return (myPow(x,  n/ 2) * myPow(x, n / 2));
-    //     } else {
-    //         return (x * myPow(x, n / 2) * myPow(x, n / 2));
+    //         return ans*ans;
     //     }
-        
+    //     return x*ans*ans;
     // }
+    public double myPow(double x,int n){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            n=-n;
+            x=1/x;
+            return x*myPow(x,n-1);
+        }
+        if(n%2==0){
+            return (myPow(x,  n/ 2) * myPow(x, n / 2));
+        } else {
+            return (x * myPow(x, n / 2) * myPow(x, n / 2));
+        }
+        
+    }
 }
