15e4343ee70b49e9901c6f532f2d2b77ed40c7fb
Mayank8881 <mayankmokhere2527@gmail.com>
2025-01-30 15:50:46 +0530

    Add Subsets

--- Subsets.java
+++ Subsets.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<(1<<nums.length);i++){
             List<Integer> list=new ArrayList<>();
             for(int j=0;j<nums.length;j++){
-                if((i & (1<<j)) !=0){
+                if((i & (1<<j)) ==1){
                     list.add(nums[j]);
                 }
             }
