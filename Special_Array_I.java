ea41a279e5536f01e819bcb8d5b4f2c74a775294
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-01 22:20:15 +0530

    Add Special Array I

--- Special_Array_I.java
+++ Special_Array_I.java
@@ -1,10 +1,11 @@
 class Solution {
-    public boolean isArraySpecial(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for(int i = 0; i < nums.size() - 1; i++) {
             if (nums[i] % 2 == nums[i + 1] % 2) {
                 return false;
             }
         }
         return true;
     }
-}
+};
