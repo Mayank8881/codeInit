815836704804b54e3b71d376272e624da2c9a2c9
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-09 09:24:55 +0530

    Add Combination Sum

---
+++ Combination_Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    private void findCombination(int i,int arr[],int target,List<List<Integer>> ans,List<Integer> ds){
+        if(i==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        if(arr[i]<=target){
+            ds.add(arr[i]);
+            findCombination(i,arr,target-arr[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        findCombination(i+1,arr,target,ans,ds);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        findCombination(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+}
