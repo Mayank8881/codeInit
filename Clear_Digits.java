70f0e4e21578d678c3afe04d081ef92cf9fe88e5
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-10 15:51:59 +0530

    Add Clear Digits

--- Clear_Digits.java
+++ Clear_Digits.java
@@ -1,10 +1,8 @@
 class Solution {
     public String clearDigits(String s) {
         StringBuilder sb=new StringBuilder("");
-        System.out.println(sb);
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>=48 && s.charAt(i)<=57){
-                // sb.delete(CharAt(i));
+        for(int i=0;i<s.length();i++){              //for(char c : s.toCharArray())
+            if(s.charAt(i)>=48 && s.charAt(i)<=57){ //if(Character.isDigit(c))
                 sb.deleteCharAt(sb.length()-1);
             }
             else{
