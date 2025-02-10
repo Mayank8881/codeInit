b80d8e4054862270f27f5de40417a76bf681838f
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-10 16:49:20 +0530

    Add Letter Combinations of a Phone Number

--- Letter_Combinations_of_a_Phone_Number.java
+++ Letter_Combinations_of_a_Phone_Number.java
@@ -1,9 +1,6 @@
 class Solution {
     public static String[] arr = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
-        if(digits.length()==0){
-            return list;
-        }
         List<String> list=new ArrayList<>();
         printDial(digits,0,new StringBuilder(),list);
         return list;
@@ -14,6 +11,7 @@ class Solution {
             list.add(sb.toString());
             return;
         }
+        
         for (char ch:arr[digits.charAt(i) - '0'].toCharArray()) {
             sb.append(ch);
             printDial(digits,i+1,sb,list);
