b4ac27a75fdbe62dd63af85ace42ea568de789b4
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-10 14:23:31 +0530

    Add Combination Sum II

--- Combination_Sum_II.java
+++ Combination_Sum_II.java
@@ -1,23 +1,40 @@
 class Solution {
-    private void findCombination(int i,int arr[],int target,Set<List<Integer>> ans,List<Integer> ds){
-        if(i==arr.length){
-            if(target==0){
-                ans.add(new ArrayList<>(ds));
-            }
-            return;
+    // private void findCombination(int i,int arr[],int target,Set<List<Integer>> ans,List<Integer> ds){
+    //     if(i==arr.length){
+    //         if(target==0){
+    //             ans.add(new ArrayList<>(ds));
+    //         }
+    //         return;
+    //     }
+    //     if(arr[i]<=target){
+    //         ds.add(arr[i]);
+    //         findCombination(i+1,arr,target-arr[i],ans,ds);
+    //         ds.remove(ds.size()-1);
+    //     }
+    //     findCombination(i+1,arr,target,ans,ds);
+    // }
+    private void findCombination(int i,int arr[],int target,List<List<Integer>> ans,List<Integer> ds){
+        if(target==0){
+            ans.add(new ArrayList<>(ds));
         }
-        if(arr[i]<=target){
-            ds.add(arr[i]);
-            findCombination(i+1,arr,target-arr[i],ans,ds);
+        
+        for(int idx=i;idx<arr.length;idx++){
+            if(idx>i && arr[idx]==arr[idx-1]){
+                continue;
+            }
+            if(arr[idx]>target){
+                break;
+            }
+            ds.add(arr[idx]);
+            findCombination(idx+1,arr,target-arr[idx],ans,ds);
             ds.remove(ds.size()-1);
         }
-        findCombination(i+1,arr,target,ans,ds);
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        Set<List<Integer>> ans=new HashSet<>();
+        List<List<Integer>> ans=new ArrayList<>();
         findCombination(0,candidates,target,ans,new ArrayList<>());
-        return new ArrayList<>(ans);
+        return ans;
     }
 }
