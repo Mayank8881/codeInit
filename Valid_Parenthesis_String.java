6b421c3d19ebc86487af8bbbc3bd07dcdc1e6215
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-18 21:16:01 +0530

    Add Valid Parenthesis String

--- Valid_Parenthesis_String.java
+++ Valid_Parenthesis_String.java
@@ -1,25 +1,6 @@
 class Solution {
     public boolean checkValidString(String s) {
-        // return check(s,0,0);
-        int min=0,max=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
-                min=min+1;
-                max=max+1;
-            }
-            else if(s.charAt(i)==')'){
-                min=min-1;
-                max=max-1;
-            }
-            else{
-                min=min-1;
-                max=max+1;
-            }
-            if(min<0)min=0;
-            if(max<0)return false;
-
-        }
-        return (min==0);
+        return check(s,0,0);
     }
     public boolean check(String s,int index,int count){
         if(count<0){
