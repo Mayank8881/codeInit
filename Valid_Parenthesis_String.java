4f1f3fe642a35d7ea213d2ffdeddf28506b2d5fc
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-20 16:42:38 +0530

    Add Valid Parenthesis String

--- Valid_Parenthesis_String.java
+++ Valid_Parenthesis_String.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean checkValidString(String s) {
-        return check(s,0,0);
-        // int min=0,max=0;
-        // for(int i=0;i<s.length();i++){
-        //     if(s.charAt(i)=='('){
-        //         min=min+1;
-        //         max=max+1;
-        //     }
-        //     else if(s.charAt(i)==')'){
-        //         min=min-1;
-        //         max=max-1;
-        //     }
-        //     else{
-        //         min=min-1;
-        //         max=max+1;
-        //     }
-        //     if(min<0)min=0;
-        //     if(max<0)return false;
+        // return check(s,0,0);
+        int min=0,max=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                min=min+1;
+                max=max+1;
+            }
+            else if(s.charAt(i)==')'){
+                min=min-1;
+                max=max-1;
+            }
+            else{
+                min=min-1;
+                max=max+1;
+            }
+            if(min<0)min=0;
+            if(max<0)return false;
 
-        // }
-        // return (min==0);
+        }
+        return (min==0);
     }
     public boolean check(String s,int index,int count){
         if(count<0){
@@ -39,7 +39,7 @@ class Solution {
             return check( s,index+1,count-1);
         }
         else {
-            return (check( s,index-1,count-1) || check( s,index+1,count-1) || check( s,index+1,count+1)) ;
+            return (check( s,index+1,count-1) || check( s,index+1,count-1) || check( s,index+1,count+1)) ;
         }
     }
 }
