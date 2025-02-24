c3d56f174766f1820423a544e0363b9e274ec012
Mayank8881 <mayankmokhere2527@gmail.com>
2025-02-24 16:52:19 +0530

    Add Jump Game

--- Jump_Game.java
+++ Jump_Game.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxIndex=-1;
+        int maxIndex=0;
         if(nums.length==1 && nums[0]!=0){
             return true;
         }
         for(int i=0;i<nums.length-1;i++){
-            if(maxIndex==0){
-                return false;
-            }
             maxIndex=Math.max(maxIndex,i+nums[i]);
         }
         if(maxIndex>=nums.length-1){
