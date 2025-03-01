39d381a8e54da67e2f06dfaa22cb9b9e60b013ea
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-01 16:23:53 +0530

    Add Apply Operations to an Array

--- Apply_Operations_to_an_Array.java
+++ Apply_Operations_to_an_Array.java
@@ -7,25 +7,16 @@ class Solution {
                 nums[i+1]=0;
             }
         }
-        List<Integer> arr = new ArrayList<>();
-        for (int num : nums) {
-            arr.add(num);
-        }
-
-        int count = 0;
-        Iterator<Integer> it = arr.iterator();
-        while(it.hasNext()) {
-            if (it.next() == 0) {
-                it.remove();
-                count++;
+        int index = 0;
+        for (int i=0; i<nums.length;i++) {
+            if (nums[i] != 0) {
+                nums[index]=nums[i];
+                index++;
             }
         }
-        for(int i=0;i<count;i++){
-            arr.add(0);
-        }
-
-        for(int i = 0;i<arr.size();i++) {
-            nums[i] = arr.get(i);
+        while (index<nums.length) {
+            nums[index] = 0;
+            index++;
         }
         return nums;
     }
