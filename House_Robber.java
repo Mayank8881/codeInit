692b76609f93780ef37554bc98373269f497d57d
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-02 15:03:24 +0530

    Add House Robber

--- House_Robber.java
+++ House_Robber.java
@@ -1,21 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int prev1=nums[0];
-        int prev2=0;
+        int dp[]=new int[n];
+        for(int i=0;i<n;i++){
+            dp[i]=-1;
+        }
+        int neg=0;
+        dp[0]=nums[0];
 
         for(int i=1;i<n;i++){
             int pick=nums[i];
             if(i>1){
-                pick+=prev2;
+                pick+=dp[i-2];
             }
             
-            int nonPick=prev1;
+            int nonPick=dp[i-1];
 
-            prev2=prev1;
-            prev1=Math.max(pick,nonPick);
+            dp[i]=Math.max(pick,nonPick);
         }
-        return prev1;
+        return dp[n-1];
     }
-
+ 
 }
