0a2716088111de5b4a48d9c689edef5dc6fd8f71
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-11 18:29:53 +0530

    Add Number of Substrings Containing All Three Characters

---
+++ Number_of_Substrings_Containing_All_Three_Characters.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int numberOfSubstrings(String s) {
+        // int count=0;
+        // for(int i=0;i<s.length();i++){
+        //     HashMap<Character,Integer> map=new HashMap<>();
+        //     for(int j=i;j<s.length();j++){
+        //         map.put(s.charAt(j),1);
+        //         if(map.containsKey('a') && map.containsKey('b') && map.containsKey('c')){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        int count=0;
+        int arr[]={-1,-1,-1};
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']=i;
+            if(arr[0]!=-1 && arr[1]!=-1 && arr[2]!=-1){
+                count+=1+Math.min(arr[0],Math.min(arr[1],arr[2]));
+            }
+        }
+        return count;
+    }
+}
