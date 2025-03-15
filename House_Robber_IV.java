eb338eaaeaa4acd40b5ff45e2d1d4bc943aa762c
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-15 15:49:35 +0530

    Add House Robber IV

---
+++ House_Robber_IV.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    //Binary Search on Answer Concept
+    public int minCapability(int[] nums, int k) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
+        for(int i : nums){
+            if(i<min){
+                min=i;
+            }
+            if(i>max){
+                max=i;
+            }
+        }
+        int start=min;
+        int end=max;
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isRobbery(mid,nums,k)){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isRobbery(int cap,int nums[],int k){
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=cap){
+                count++;
+                i++;
+            }
+            if(count>=k){
+                return true;
+            }
+        }
+        return false;
+    }
+}
