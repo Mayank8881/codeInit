e6e2c641008ff277aa2899da597c3674c709cba9
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-17 15:30:30 +0530

    Add Unique Paths

--- Unique_Paths.java
+++ Unique_Paths.java
@@ -1,20 +1,22 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
-        dp[0][0]=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                else{
-                    int left=0,up=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-
-                    dp[i][j]=up+left;
-                }
-                
+                dp[i][j]=-1;
             }
         }
-        return dp[m-1][n-1];
+        return paths(m-1,n-1,dp);
+    }
+    public int paths(int m,int n,int dp[][]){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+
+        if(dp[m][n]!=-1) return dp[m][n];
+
+        int up=paths(m-1,n,dp);
+        int left=paths(m,n-1,dp);
+
+        return dp[m][n]=up+left;
     }
 }
