b9cfd6ab3e1ff55eda1ff8703215f1b23a690e1d
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-19 16:13:22 +0530

    Add Minimum Falling Path Sum

--- Minimum_Falling_Path_Sum.java
+++ Minimum_Falling_Path_Sum.java
@@ -3,24 +3,14 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
         int dp[][]=new int[n][m];
-        for(int j=0;j<m;j++){
-            dp[0][j]=matrix[0][j];
-        }
-
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                int left=Integer.MAX_VALUE,right=Integer.MAX_VALUE;
-                int up=matrix[i][j]+dp[i-1][j];
-                if(j-1>0) left=matrix[i][j]+dp[i-1][j-1];
-                if(j+1<m) right=matrix[i][j]+dp[i-1][j+1];
-
-                dp[i][j]=Math.min(up,Math.min(left,right));
+                dp[i][j]=-1;
             }
         }
-
         int maxi=Integer.MAX_VALUE;
         for(int j=0;j<m;j++){
-            maxi=Math.min(maxi,dp[n-1][j]);
+            maxi=Math.min(maxi,paths(n-1,j,matrix,dp));
         }
         return maxi;
     }
