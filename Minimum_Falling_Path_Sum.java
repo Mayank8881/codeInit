2218637a98f7bb9a76646667a37d349426800784
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-19 16:20:51 +0530

    Add Minimum Falling Path Sum

--- Minimum_Falling_Path_Sum.java
+++ Minimum_Falling_Path_Sum.java
@@ -2,23 +2,38 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-
-        int mini=Integer.MAX_VALUE;
+        int dp[][]=new int[n][m];
         for(int j=0;j<m;j++){
-            mini=Math.min(mini,paths(n-1,j,matrix));
+            dp[0][j]=matrix[0][j];
         }
-        return mini;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int left=Integer.MAX_VALUE,right=Integer.MAX_VALUE;
+                int up=matrix[i][j]+dp[i-1][j];
+                if(j>0) left=matrix[i][j]+dp[i-1][j-1];
+                if(j<m-1) right=matrix[i][j]+dp[i-1][j+1];
+
+                dp[i][j]=Math.min(up,Math.min(left,right));
+            }
+        }
+
+        int maxi=Integer.MAX_VALUE;
+        for(int j=0;j<m;j++){
+            maxi=Math.min(maxi,dp[n-1][j]);
+        }
+        return maxi;
     }
 
-    public int paths(int i,int j,int arr[][]){
+    public int paths(int i,int j,int arr[][],int dp[][]){
         if(j<0 || j>=arr[0].length) return Integer.MAX_VALUE;
         if(i==0) return arr[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=paths(i-1,j,arr,dp);
+        int left=paths(i-1,j-1,arr,dp);
+        int right=paths(i-1,j+1,arr,dp);
 
-        int up=paths(i-1,j,arr);
-        int left=paths(i-1,j-1,arr);
-        int right=paths(i-1,j+1,arr);
-
-        int mini = Math.min(up, Math.min(left, right));
-        return (mini == Integer.MAX_VALUE) ? arr[i][j] : arr[i][j] + mini;
+        int minPath = Math.min(up, Math.min(left, right));
+        return dp[i][j]=(minPath == Integer.MAX_VALUE) ? arr[i][j] : arr[i][j] + minPath;
     }
 }
