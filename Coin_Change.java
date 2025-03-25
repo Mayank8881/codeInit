deca4514d15424ea9c59a4238c6c5f27112ee1f1
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-25 20:26:27 +0530

    Add Coin Change

--- Coin_Change.java
+++ Coin_Change.java
@@ -7,28 +7,9 @@ class Solution {
             Arrays.fill(row, -1);
         }
 
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0) dp[0][i] = i/coins[0];
-            else dp[0][i] = inf;
-        }
-
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                int nonPick=dp[i-1][j];
-                int pick=Integer.MAX_VALUE;
-                if(coins[i]<=j){
-                    pick=1+dp[i][j-coins[i]];
-                }
-                dp[i][j]=Math.min(pick,nonPick);
-            }
-        }
-        int ans=dp[n-1][amount];
+        int ans=path(coins,coins.length-1,amount,dp);
         if(ans>=inf) return -1;
         return ans;
-
-        // int ans=path(coins,coins.length-1,amount,dp);
-        // if(ans>=inf) return -1;
-        // return ans;
     }
     public int path(int[] coin,int i,int amount,int dp[][]){
         int inf = (int) 1e9;
