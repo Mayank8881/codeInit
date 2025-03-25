b8f2be86402de9ebd4a085396884fa0347ad096c
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-25 20:14:48 +0530

    Add Coin Change

--- Coin_Change.java
+++ Coin_Change.java
@@ -1,17 +1,11 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int inf = (int) 1e9;
-        int ans=path(coins,coins.length-1,amount);
-        if(ans>=inf){
-            return -1;
-        }
-        return ans;
+        return path(coins,coins.length-1,amount);
     }
     public int path(int[] coin,int i,int amount){
-        int inf = (int) 1e9;
         if(i==0){
             if(amount%coin[i]==0) return amount/coin[i];
-            else return inf;
+            else return -1;
         }
 
         int nonPick=path(coin,i-1,amount);
