b73dcee79c3cfa5b2db6199c772f6579c8159c64
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-26 17:46:39 +0530

    Add Coin Change II

--- Coin_Change_II.java
+++ Coin_Change_II.java
@@ -1,29 +1,22 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
-        int dp[][]=new int[n][amount+1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-
-        int ans=path(coins,coins.length-1,amount,dp);
+        int ans=path(coins,coins.length-1,amount);
         return ans;
     }
-    public int path(int[] coin,int i,int amount,int dp[][]){
+    public int path(int[] coin,int i,int amount){
         int inf = (int) 1e9;
         if(i==0){
             if(amount%coin[i]==0) return 1;
             else return 0;
         }
 
-        if(dp[i][amount]!=-1) return dp[i][amount];
-
-        int nonPick=path(coin,i-1,amount,dp);
+        int nonPick=path(coin,i-1,amount);
         int pick=0;
         if(coin[i]<=amount){
-            pick=path(coin,i,amount-coin[i],dp);
+            pick=path(coin,i,amount-coin[i]);
         }
 
-        return dp[i][amount]=pick+nonPick;
+        return pick+nonPick;
     }
 }
