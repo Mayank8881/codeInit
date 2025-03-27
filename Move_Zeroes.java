6112b9d1288dfef16f12ab32634b625aa9914c01
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-27 19:27:02 +0530

    Add Move Zeroes

--- Move_Zeroes.java
+++ Move_Zeroes.java
@@ -1,25 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-
-        if(j==-1){
-            return;
-        }
-        int temp=0;
-        for(int i=j+1;i<nums.length;i++){
+        int n=nums.length;
+        int j=0;
+        for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+                if(i!=j){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
                 j++;
             }
         }
-
     }
 }
