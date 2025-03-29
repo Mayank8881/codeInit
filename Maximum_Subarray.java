706fff58be900fce4ae14db6b118117e096b56a1
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-29 18:20:19 +0530

    Add Maximum Subarray

--- Maximum_Subarray.java
+++ Maximum_Subarray.java
@@ -1,11 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length-1;i++){
-            int sum=0;
-            for(int j=i;j<nums.length-1;i++){
-                sum+=nums[j];
-                maxSum=Math.max(sum,maxSum);
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>maxSum){
+                maxSum=sum;
+            }
+
+            if(sum<0){
+                sum=0;
             }
         }
         return maxSum;
