8ca55a76c941953c0aac60d6b305138febd83a80
Mayank8881 <mayankmokhere2527@gmail.com>
2025-03-30 14:51:31 +0530

    Add Partition Labels

--- Partition_Labels.java
+++ Partition_Labels.java
@@ -2,32 +2,24 @@ import java.util.*;
 
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int[] freq = new int[26];
-        int a = 'a';
-
-        for (char c : s.toCharArray()) {
-            freq[c - a]++;
-        }
-
-        List<Integer> output = new ArrayList<>();
-        int prevEnd = 0;
+        int[] last = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - a]--;
+            last[s.charAt(i) - 'a'] = i;
+        }
 
-            if (isValid(prevEnd, i, s, freq)) {
-                output.add(i - prevEnd + 1);
-                prevEnd = i + 1;
+        List<Integer> result = new ArrayList<>();
+        int maxLast = 0, prevEnd = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            maxLast = Math.max(maxLast, last[s.charAt(i) - 'a']);
+
+            if (i == maxLast) {
+                result.add(i - prevEnd);
+                prevEnd = i;
             }
         }
 
-        return output;
-    }
-
-    private boolean isValid(int start, int end, String s, int[] freq) {
-        for (int i = start; i <= end; i++) {
-            if (freq[s.charAt(i) - 'a'] != 0) return false;
-        }
-        return true;
+        return result;
     }
 }
