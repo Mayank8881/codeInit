23b5d9a4c4e223219b612ca80cf5d9b32d40155d
Mayank8881 <mayankmokhere2527@gmail.com>
2025-04-02 18:22:39 +0530

    Add Maximum Value of an Ordered Triplet I

--- Maximum_Value_of_an_Ordered_Triplet_I.java
+++ Maximum_Value_of_an_Ordered_Triplet_I.java
@@ -1,12 +1,11 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long maxSum=0;
+        int maxSum=0;
         int n=nums.length;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
                 for(int k=j+1;k<n;k++){
-                    long sum=((long) nums[i]-nums[j])*nums[k];
-                    System.out.println(sum);
+                    int sum=(nums[i]-nums[j])*nums[k];
                     if(sum>maxSum) maxSum=sum;
                 }
             }
