6e184fdb5db5b39cf70ce6ab7394f02ca5f2a36d
Mayank8881 <mayankmokhere2527@gmail.com>
2025-04-04 13:52:32 +0530

    Add Longest Common Subsequence

--- Longest_Common_Subsequence.java
+++ Longest_Common_Subsequence.java
@@ -2,23 +2,13 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n=text1.length();
         int m=text2.length();
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return lcs(text1,text2,n-1,m-1,dp);
+        return lcs(text1,text2,n-1,m-1);
     }
 
-    public int lcs(String s1,String s2,int n,int m,int dp[][]){
+    public int lcs(String s1,String s2,int n,int m){
         if(m<0 || n<0) return 0;
 
-        if(dp[n][m]!=-1) return dp[n][m];
-
-        if(s1.charAt(n)==s2.charAt(m))  dp[n][m]=1+lcs(s1,s2,n-1,m-1,dp);
-        else  dp[n][m]=0+Math.max(lcs(s1,s2,n-1,m,dp),lcs(s1,s2,n,m-1,dp));
-
-        return dp[n][m];
+        if(s1.charAt(n)==s2.charAt(m)) return 1+lcs(s1,s2,n-1,m-1);
+        else return 0+Math.max(lcs(s1,s2,n-1,m),lcs(s1,s2,n,m-1));
     }
 }
