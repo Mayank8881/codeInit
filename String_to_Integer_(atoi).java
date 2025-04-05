ee5688efa3b1873916c1fb38e7739cab09d81e14
Mayank8881 <mayankmokhere2527@gmail.com>
2025-04-05 19:02:37 +0530

    Add String to Integer (atoi)

--- String_to_Integer_(atoi).java
+++ String_to_Integer_(atoi).java
@@ -7,7 +7,7 @@ class Solution {
         }
 
         int sign=1;
-        if(i<n &&s.charAt(i)=='-' || s.charAt(i)=='+'){
+        if(i<n && (s.charAt(i)=='-' || s.charAt(i)=='+')){
             if(s.charAt(i)=='-'){
                 sign=-1;
             }
@@ -24,5 +24,41 @@ class Solution {
             i++;
         }
         return sign*num;
+    }
+
+    // public int myAtoi(String s) {
+    //     s=s.trim();
+    //     if(s.isEmpty()){
+    //         return 0;
+    //     }
+
+    //     boolean negative=false;
+    //     int index=0;
+
+    //     if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+    //         if( s.charAt(0) == '-'){
+    //             negative=true;
+    //         }
+    //         index++;
+    //     }
+
+    //     return helper(s,index,negative,0);
+    // }
+
+    public static int helper(String s,int index,boolean negative,int result){
+        
+        if(index >= s.length() || !Character.isDigit(s.charAt(index))) {
+            return negative ? -result : result;
+        }
+        
+        int digit=s.charAt(index)-'0';
+
+        if (result > (Integer.MAX_VALUE - digit) / 10) {
+            return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+
+        result=result*10+digit;
+        return helper(s,index+1,negative,result);
+
     }
 }
