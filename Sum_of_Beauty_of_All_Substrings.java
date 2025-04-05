c952504316b0bf6034eb89c64f6551cf293c14f8
Mayank8881 <mayankmokhere2527@gmail.com>
2025-04-05 20:37:48 +0530

    Add Sum of Beauty of All Substrings

--- Sum_of_Beauty_of_All_Substrings.java
+++ Sum_of_Beauty_of_All_Substrings.java
@@ -1,18 +1,23 @@
 class Solution {
     public int beautySum(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        
+        int ans=0;        
         for(int i=0;i<s.length();i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-        }
-        int maxCount=Integer.MIN_VALUE;
-        int minCount=Integer.MAX_VALUE;
-        for(int i=0;i<s.length();i++){
-            int count=map.get(s.charAt(i));
-            minCount=Math.min(minCount,count);
-            maxCount=Math.max(maxCount,count);
-        }
+            HashMap<Character,Integer> map=new HashMap<>();
+            
+            for(int j=i;j<s.length();j++){
+                map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+                int maxCount=Integer.MIN_VALUE;
+                int minCount=Integer.MAX_VALUE;
+                
+                for (int freq : map.values()) {
+                    minCount = Math.min(minCount, freq);
+                    maxCount = Math.max(maxCount, freq);
+                }
+                ans+=maxCount-minCount;
 
-        return max-min;
+            }
+            
+        }
+        return ans;
     }
 }
