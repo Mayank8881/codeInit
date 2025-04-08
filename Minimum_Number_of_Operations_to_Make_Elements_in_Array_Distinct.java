a0e80e9b331f0420130f63d3a68147243efac3ad
Mayank8881 <mayankmokhere2527@gmail.com>
2025-04-08 18:08:59 +0530

    Add Minimum Number of Operations to Make Elements in Array Distinct

--- Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.java
+++ Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.java
@@ -1,16 +1,22 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Map<Integer, Integer> last = new HashMap<>();
-        int res = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (last.containsKey(num) && last.get(num) >= 3 * res) {
-                res = (last.get(num) + 1 + 2) / 3; // Simulate Math.ceil
-            }
-            last.put(num, i);
+        int cnt = 0, temp;
+        List<Integer> list = new ArrayList<>();
+        for (int n : nums)
+            list.add(n);
+        while (true) {
+            HashMap<Integer, Integer> mpp = new HashMap<>();
+            temp = 0;
+            for (int num : list)
+                if (mpp.put(num, mpp.getOrDefault(num, 0) + 1) != null && mpp.get(num) == 2)
+                    temp++;
+            if (temp == 0)
+                break;
+            int removeCount = Math.min(3, list.size());
+            for (int i = 0; i < removeCount; i++)
+                list.remove(0);
+            cnt++;
         }
-
-        return res;
+        return cnt;
     }
 }
