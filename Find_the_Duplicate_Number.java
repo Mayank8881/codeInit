ccbfd71b39ee7235ebd34e0d97fc4ef8d6391a25
Mayank8881 <mayankmokhere2527@gmail.com>
2025-05-13 21:43:18 +0530

    Add Find the Duplicate Number

--- Find_the_Duplicate_Number.java
+++ Find_the_Duplicate_Number.java
@@ -1,4 +1,6 @@
 class Solution {
+
+    //linked list slow fast pointer usage
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
         int fast = nums[0];
@@ -11,7 +13,6 @@ class Solution {
                 break;
             }
         }
-
         int slow2 = nums[0];
 
         while (slow != slow2) {
