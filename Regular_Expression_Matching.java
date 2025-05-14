90bf3308f348c85737486d683eab5d5772b84b32
Mayank8881 <mayankmokhere2527@gmail.com>
2025-05-14 21:10:18 +0530

    Add Regular Expression Matching

--- Regular_Expression_Matching.java
+++ Regular_Expression_Matching.java
@@ -2,32 +2,25 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n=s.length();
         int m=p.length();
-        Boolean[][] dp = new Boolean[n][m];
-        return findMatch(s,p,n-1,m-1,dp);
+        Boolean[][] dp = new Boolean[n+1][m+1];
+        return findMatch(s,p,n,m,dp);
     }
 
     public boolean findMatch(String s,String p,int i,int j,Boolean[][] dp){
+        if (i == 0 && j == 0) return true;
+        if (j == 0) return false;
+        if (dp[i][j] != null) return dp[i][j];
 
-        if(i<0 && j<0) return true;
-        if(j<0 && i>=0) return false;
-        if(i<0 && j>=0){
-            for(int k=0;k<=j;k++){
-                if(p.charAt(k)!='*') return false;
-            }
-            return true;
+        boolean match;
+        if (j >= 2 && p.charAt(j - 1) == '*') {
+            match = findMatch(s, p, i, j - 2, dp) || 
+                    (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') 
+                     && findMatch(s, p, i - 1, j, dp));
+        } else {
+            match = i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') 
+                    && findMatch(s, p, i - 1, j - 1, dp);
         }
-
-        if(dp[i][j]!=null ) return dp[i][j];
-
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='.'){
-            dp[i][j]=findMatch(s,p,i-1,j-1,dp);
-        }
-        else if(p.charAt(j)=='*'){
-            dp[i][j]= findMatch(s,p,i,j-1,dp) || findMatch(s,p,i-1,j,dp);
-        }
-        else{
-            dp[i][j]=false;
-        }
-        return dp[i][j];
+        dp[i][j] = match;
+        return match;
     }
 }
