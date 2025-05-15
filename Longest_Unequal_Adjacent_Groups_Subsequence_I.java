490bfee7ea20e387f6cf7c8b838a7b0012cbeae9
Mayank8881 <mayankmokhere2527@gmail.com>
2025-05-15 13:57:10 +0530

    Add Longest Unequal Adjacent Groups Subsequence I

--- Longest_Unequal_Adjacent_Groups_Subsequence_I.java
+++ Longest_Unequal_Adjacent_Groups_Subsequence_I.java
@@ -3,12 +3,13 @@ class Solution {
         List<String> list=new ArrayList<>();
         boolean isPick=true;
         list.add(words[0]);
-        int last=groups[0];
         
         for(int i=1;i<groups.length;i++){
-            if(groups[i]==last){
+            if(groups[i]==groups[i-1]){
+                isPick=!isPick;
+            }
+            if(isPick){
                 list.add(words[i]);
-                last=groups[i];
             }
         }
         return list;
