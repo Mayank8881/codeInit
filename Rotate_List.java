573b2860b12849312f5a3281b0f874097fadfa64
Mayank8881 <mayankmokhere2527@gmail.com>
2025-05-16 20:18:56 +0530

    Add Rotate List

--- Rotate_List.java
+++ Rotate_List.java
@@ -10,6 +10,29 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
         
+        ListNode temp = head;
+        int length = 0;
+        while(temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        int rotation = k % length;
+
+        for(int i = 0; i < rotation; i++) {
+            ListNode last = head, prev = null;
+            
+            while(last.next != null) {
+                prev = last;
+                last = last.next;
+            }
+            
+            last.next = head;
+            prev.next = null;
+            head = last;
+        }
+        
+        return head;
     }
 }
