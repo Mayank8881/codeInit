61827b57355decc7835f245f356545a4b19c238b
Mayank8881 <mayankmokhere2527@gmail.com>
2025-05-16 21:57:32 +0530

    Add Validate Binary Search Tree

---
+++ Validate_Binary_Search_Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return checkBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    boolean checkBST(TreeNode root,long mini,long maxi){
+        if(root==null) return true;
+        if(root.val<=mini || root.val>=maxi) return false;
+
+        boolean left=checkBST(root.left,mini,root.val);
+        boolean right=checkBST(root.right,root.val,maxi);
+
+        return left&&right;
+    }
+}
