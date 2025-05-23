56f174a386360012cf577689b06800e2de34a74c
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-05-24 02:03:32 +0530

    0061-rotate-list Medium .java 0 ms (100%)

--- LeetCode/Medium/0061-rotate-list/0061-rotate-list.java
+++ LeetCode/Medium/0061-rotate-list/0061-rotate-list.java
@@ -10,6 +10,28 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode temp=head;
+        int length=1;
+
+        while(temp.next!=null){
+            length++;
+            temp=temp.next;
+        }
+
+        temp.next=head;
+        k=k%length;
+
+        ListNode last=head;
+        for(int i=0;i<length-k-1;i++){
+            last=last.next;
+        }
+
+        ListNode newHead = last.next;
+        last.next = null;
+
+        return newHead;
         
     }
 }
