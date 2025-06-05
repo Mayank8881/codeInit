6faf18df9e20dbab2c68f7080bcb33714f213cdd
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-06-05 13:59:31 +0530

    2104-sum-of-subarray-ranges Medium .java 42 ms (23.37%)

--- LeetCode/Medium/2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ LeetCode/Medium/2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,17 +1,86 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        int n=nums.length;
-        long sum=0;
-        for(int i=0;i<n;i++){
-            int l=nums[i],s=nums[i];
-            for(int j=i+1;j<n;j++){
-                l=Math.max(l,nums[j]);
-                s=Math.min(s,nums[j]);
+        int nse[]=nse(nums);
+        int pse[]=pse(nums);
+        int nge[]=nge(nums);
+        int pge[]=pge(nums);
+        long total=0;
+        
+        for (int i=0;i<nums.length;i++){
+            long maxCount=(long)(i-pge[i])*(nge[i]-i);
+            long minCount=(long)(i-pse[i])*(nse[i]-i);
 
-                sum+=(long)l-s;
-
-            }
+            total +=(long)nums[i]*(maxCount - minCount);
         }
-        return sum;
+        return total;
+    }
+
+    public int[] nge(int arr[]){
+        int a[]=new int[arr.length];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=a.length-1;i>=0;i--){
+            while(!stack.isEmpty() && arr[stack.peek()]<=arr[i]){
+                stack.pop();
+            }
+
+            a[i]= stack.isEmpty()? a.length:stack.peek();
+
+            stack.push(i);
+
+        }
+        return a;
+    }
+
+
+
+    public int[] nse(int arr[]){
+        int a[]=new int[arr.length];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=a.length-1;i>=0;i--){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+                stack.pop();
+            }
+
+            a[i]= stack.isEmpty()? a.length:stack.peek();
+
+            stack.push(i);
+
+        }
+        return a;
+    }
+
+    public int[] pse(int arr[]){
+        int a[]=new int[arr.length];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=0;i<a.length;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
+                stack.pop();
+            }
+
+            a[i]= stack.isEmpty()? -1:stack.peek();
+
+            stack.push(i);
+
+        }
+        return a;
+    }
+    public int[] pge(int arr[]){
+        int a[]=new int[arr.length];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=0;i<a.length;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]<arr[i]){
+                stack.pop();
+            }
+
+            a[i]= stack.isEmpty()? -1:stack.peek();
+
+            stack.push(i);
+
+        }
+        return a;
     }
 }
