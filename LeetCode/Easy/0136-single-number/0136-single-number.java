4d8cc6dbe5a814aa84139788212666ce3f1168da
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-06-08 01:40:11 +0530

    0136-single-number Easy .java 1 ms (99.93%)

--- LeetCode/Easy/0136-single-number/0136-single-number.java
+++ LeetCode/Easy/0136-single-number/0136-single-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int xor=0;
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            xor=xor^nums[i];
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])==1) return nums[i];
-        }
-        return -1;
+        return xor;
     }
 }
