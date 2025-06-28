7cf3457851e40150f9285a7641123a32d8c8a626
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-06-29 01:25:46 +0530

    0062-unique-paths Medium .java 0 ms (100%)

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                else{
-                    int left=0,up=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-
-                    dp[i][j]=up+left;
-                }
-                
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j]=-1;
             }
         }
-        return dp[m-1][n-1];
+        return minPath(m,n,dp);
+    }
+
+    public int minPath(int m,int n,int[][] dp){
+        if(m==1 && n==1) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+
+        int top=0;
+        int left=0;
+        if(m>0){
+            top=minPath(m-1,n,dp);
+        }
+        if(n>0){
+            left=minPath(m,n-1,dp);
+        }
+        dp[m][n]=top+left;
+        return dp[m][n];
     }
 }
