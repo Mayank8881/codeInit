927ebb8841d60040ee3069f1038306d971260d3c
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-07-30 01:33:44 +0530

    2411-smallest-subarrays-with-maximum-bitwise-or Medium .java 4 ms (100%)

---
+++ LeetCode/Medium/2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] smallestSubarrays(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            int x = nums[i];
+            res[i] = 1;
+            int j = i - 1;
+            while (j >= 0 && ( (nums[j] | x) != nums[j]) ) {
+                res[j] = i - j + 1;
+                nums[j] |= x;
+                j--;
+            }
+        }
+        return res;
+    }
+}
