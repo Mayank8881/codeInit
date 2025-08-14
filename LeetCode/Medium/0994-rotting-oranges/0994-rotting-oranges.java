173b42eabf6cb7441f6623a14cef15cf1deba75d
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-08-14 13:36:51 +0530

    0994-rotting-oranges Medium .java 2 ms (66.67%)

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
@@ -8,44 +8,55 @@ class Pair{
         this.tm=tm;
     }
 }
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int vis[][]=new int[n][m];   
-        Queue<Pair> queue=new LinkedList<>();
+        Queue<Pair> q=new LinkedList<>();
+        int[][] vis=new int[n][m];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    queue.add(new Pair(i,j,0));
-                    vis[i][j]=2;
+                    q.add(new Pair(i,j,0));
+                    vis[i][j]=1;
                 }
             }
         }
 
+
+        int[] drow={0,1,0,-1};
+        int[] dcol={1,0,-1,0};
+
         int time=0;
-        int drow[]={-1,0,1,0};
-        int dcol[] ={0,1,0,-1};
-        while(!queue.isEmpty()){
-            int r=queue.peek().row;
-            int c=queue.peek().col;
-            int t=queue.peek().tm;
+
+        while(!q.isEmpty()){
+            int r=q.peek().row;
+            int c=q.peek().col;
+            int t=q.peek().tm;
+
+            q.remove();
+            
             time=Math.max(time,t);
-            queue.remove();
 
             for(int i=0;i<4;i++){
-                int row=r+drow[i];
-                int col=c+dcol[i];
-                if(row>=0 && row<n && col>=0 && col<m && vis[row][col]!=2 && grid[row][col]==1){
-                    queue.add(new Pair(row,col,t+1));
-                    vis[row][col]=2;
+                int newRow=r+drow[i];
+                int newCol=c+dcol[i];
+
+                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==1 && 
+                    vis[newRow][newCol]==0) {
+
+                        vis[newRow][newCol]=1;
+                        q.add(new Pair(newRow,newCol,t+1));
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1) return -1;
+                if(vis[i][j]!=1 && grid[i][j]==1){
+                    return -1;
+                }
             }
         }
         return time;
