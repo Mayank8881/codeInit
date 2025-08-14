461bab86f626fc7d5428626c692da13d52195325
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-08-14 14:40:49 +0530

    0733-flood-fill Easy .java 2 ms (9.11%)

--- LeetCode/Easy/0733-flood-fill/0733-flood-fill.java
+++ LeetCode/Easy/0733-flood-fill/0733-flood-fill.java
@@ -1,50 +1,53 @@
 class Pair{
-    int row;
-    int col;
-    int tm;
+    int row,col;
     Pair(int row,int col){
         this.row=row;
         this.col=col;
     }
 }
-
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n=image.length;
         int m=image[0].length;
         Queue<Pair> q=new LinkedList<>();
-        int[][] vis=new int[n][m];
         int[][] res=new int[n][m];
+
+        int[][] vis=new int[n][m];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 res[i][j]=image[i][j];
             }
         }
+        int clr=res[sr][sc];
         q.add(new Pair(sr,sc));
-        int val=image[sr][sc];
         res[sr][sc]=color;
         vis[sr][sc]=1;
+        
+        int[] drow={0,1,0,-1};
+        int[] dcol={1,0,-1,0};
 
-        int drow[]={-1,0,1,0};
-        int dcol[] ={0,1,0,-1};
 
         while(!q.isEmpty()){
             int r=q.peek().row;
             int c=q.peek().col;
+            q.remove();
 
             for(int i=0;i<4;i++){
-                int row=r+drow[i];
-                int col=c+dcol[i];
+                int newRow=r+drow[i];
+                int newCol=c+dcol[i];
 
-                if(row>=0 && row<n && col>=0 && col<m && image[row][col]==val && vis[row][col]!=1){
-                    q.add(new Pair(row,col));
-                    res[row][col]=color;
-                    vis[row][col] = 1;
+                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && vis[newRow][newCol]!=1 && 
+                    res[newRow][newCol]==clr){
+                        q.add(new Pair(newRow,newCol));
+                        res[newRow][newCol]=color;
+                        vis[newRow][newCol]=1;
                 }
-            }
-        }
 
+            }
+
+        }
         return res;
+
     }
 }
