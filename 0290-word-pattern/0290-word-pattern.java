8b0be353d749720afc83d98d8c3aaa3f18172795
Mayank Mokhere <134052456+Mayank8881@users.noreply.github.com>
2025-09-01 20:35:32 +0530

    Time: 0 ms (100%), Space: 42.1 MB (5.24%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String str[]=s.split(" ");
+        if (pattern.length() != str.length) return false;
+        
+        Map<Character,String> map=new HashMap<>();
+        for(int i=0;i<str.length;i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                if(map.containsValue(str[i])) return false;
+                map.put(pattern.charAt(i),str[i]);
+            }
+            else{
+                if(!map.get(pattern.charAt(i)).equals(str[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+
+        
+    }
+}
